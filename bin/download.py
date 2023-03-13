64c4b8b6bba851476e3ec24a3658051f43006e99
John-Oula <johnoula@antratechstudios.com>
2023-03-13 16:18:41 +0800

    locize setup

--- bin/download.py
+++ bin/download.py
@@ -1,7 +1,7 @@
 import subprocess
 import sys
-# Download source files through starling CLI
+# Download source files through locize CLI
 
 project_id = str(sys.argv[len(sys.argv)-1].split('=')[len(sys.argv[len(sys.argv)-1].split('='))-1])
 print('Project_id ===>', project_id)
-subprocess.run('cd %s && starling client download && cd ..' % project_id, shell=True)
+subprocess.run('cd %s && locize download && cd ..' % project_id, shell=True)
--- server/server.py
+++ server/server.py
@@ -9,23 +9,25 @@ import threading
 ACCESS_KEY = "AKLTMDc3MGY5ZmI4NDI4NDRjZmE0ZjkyMDhjZDQ0YzI0Yzg"
 SECRET_KEY = "T0RReE1EQXlZMk0wWVdNMU5ETTBZVGhsTkdFd00yVmxPVGRsWkdRMll6VQ=="
 
+
 # Run serge sync shell command
 def serge_sync():
     return subprocess.run('serge sync ../data/configs/ >../log/serge.log 2>&1', shell=True)
+
 
 # Run serge clean-ts command
 def serge_clean():
     return subprocess.run('serge clean-ts ../data/configs/ >>../log/serge.log 2>&1', shell=True)
 
+
 def serge_loop():
+    while True:
+        serge_sync()
+        serge_clean()
 
-        while True:
+        print("Waiting 10 seconds till the next cycle. Press [Ctrl+C] to stop...")
+        time.sleep(10)
 
-            serge_sync()
-            serge_clean()
-
-            print("Waiting 10 seconds till the next cycle. Press [Ctrl+C] to stop...")
-            time.sleep(10)
 
 serge = threading.Thread(target=serge_loop)
 serge.start()
